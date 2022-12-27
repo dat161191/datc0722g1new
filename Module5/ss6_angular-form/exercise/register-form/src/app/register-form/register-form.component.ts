7c4de72921b7a8e559cfaff81dfad7b25b233fc9
dat16111991 <quocdat.tran453@gmail.com>
2022-12-28 01:26:35 +0700

    Angular

--- Module5/ss6_angular-form/exercise/register-form/src/app/register-form/register-form.component.ts
+++ Module5/ss6_angular-form/exercise/register-form/src/app/register-form/register-form.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {AbstractControl, FormControl, FormGroup, Validators} from '@angular/forms';
 import {Country} from '../Country';
 
 @Component({
@@ -9,20 +9,21 @@ import {Country} from '../Country';
 })
 export class RegisterFormComponent implements OnInit {
 
+  // @ts-ignore
   registerForm = new FormGroup({
     email: new FormControl('', [Validators.required, Validators.email]),
     password: new FormControl('', [Validators.required, Validators.minLength(6)]),
-    confirmPassword: new FormControl('', [Validators.required, Validators.minLength(6)]),
-    country: new FormControl('',Validators.required),
-    age: new FormControl('',[Validators.required,Validators.min(18)]),
-    gender: new FormControl(),
+    confirmPassword: new FormControl('', [Validators.required]),
+    country: new FormControl('', Validators.required),
+    age: new FormControl('', [Validators.required, Validators.min(18)]),
+    gender: new FormControl('',Validators.required),
     phone: new FormControl('', [Validators.required, Validators.pattern('[+][8][4][ ][0-9]{9,10}')])
-  });
-  countryList: Country[]= [
-    { id: 1, name: 'VietNam' },
-    { id: 2, name: 'USA' },
-    { id: 3, name: 'Korea' },
-    { id: 4, name: 'England' }
+  }, {validators: this.checkPassword(`password`, `confirmPassword`)});
+  countryList: Country[] = [
+    {id: 1, name: 'VietNam'},
+    {id: 2, name: 'USA'},
+    {id: 3, name: 'Korea'},
+    {id: 4, name: 'England'}
   ];
 
   constructor() {
@@ -37,5 +38,21 @@ export class RegisterFormComponent implements OnInit {
 
   create() {
     console.log(this.registerForm.value);
+  }
+
+  checkPassword(controlPass: string, matchingControlConfirm: string): any {
+    return (formGroup: FormGroup) => {
+      const control = formGroup.controls[controlPass];
+      const matching = formGroup.controls[matchingControlConfirm];
+      if (matching.errors && !matching.errors.checkPassword) {
+        return;
+      }
+      if (control.value != matching.value) {
+        return matching.setErrors({checkPassword: true});
+      } else {
+        return matching.setErrors(null);
+      }
+    };
+
   }
 }
